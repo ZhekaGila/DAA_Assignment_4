21981aa1159d0bc011770a6a39420a52fcea8f21
zheka_not_fat <119170388+ZhekaGila@users.noreply.github.com>
2025-11-03 05:03:53 +0500

    fixing some bugs

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -2,18 +2,15 @@ package model;
 
 public class Node {
     private int id;
-    private long duration;
 
     public Node(int id, long duration) {
         this.id = id;
-        this.duration = duration;
     }
 
     public int getId() { return id; }
-    public long getDuration() { return duration; }
 
     @Override
     public String toString() {
-        return "Node{id=" + id + ", duration=" + duration + "}";
+        return "Node{id="id"}";
     }
 }
